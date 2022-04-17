1737dedbcefb12d7b9090ab245085eed70724d4c
Дарья Максимовна Высокова <337330@niuitmo.ru>
2022-04-17 19:02:17 +0300

    Добавьте файлы проекта.

---
+++ PoserParser/Report.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PoserParser
+{
+	/// <summary>
+	/// Логика взаимодействия для Report.xaml
+	/// </summary>
+	public partial class Report : Window
+	{
+		public Report()
+		{
+			InitializeComponent();
+		}
+
+        private void EditHistoryClick(object sender, RoutedEventArgs e)
+        {
+			EditHistory	edit = new EditHistory();
+			edit.DataContext = MainWindow.changed2;
+			edit.Show();
+        }
+
+        private void Changes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
